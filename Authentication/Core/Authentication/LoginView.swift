de17917fc641f4df5ef53049d6ba3f8b0a765289
Suayip Emre <suayipemredev@gmail.com>
2024-03-11 22:13:41 +0300

    login view

---
+++ Authentication/Core/Authentication/LoginView.swift
@@ -0,0 +1,71 @@
+//
+//  LoginView.swift
+//  Authentication
+//
+//  Created by Şuayip Emre on 11.03.2024.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    @State private var email = ""
+    @State private var passwword = ""
+    var body: some View {
+        NavigationStack{
+            VStack{
+                // image
+                Image("swiftui-logo")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 100, height: 120)
+                    .padding(.vertical, 32)
+                
+                //form fields
+                VStack(spacing:24){
+                    InputView(text: $email, title: "Email Address", placeholder: "name@example.com")
+                        .textInputAutocapitalization(.none)
+                    
+                    InputView(text: $passwword, title: "Password", placeholder: "enter your password", isSecureField: true)
+                        .textInputAutocapitalization(.none)
+                }
+                .padding(.horizontal)
+                .padding(.top, 12)
+                
+                //sign in button
+                Button(action: {
+                    print("log user in")
+                }, label: {
+                    HStack{
+                        Text("SIGN IN")
+                            .fontWeight(.semibold)
+                        Image(systemName: "arrow.right")
+                    }
+                    .frame(width: UIScreen.main.bounds.width - 32, height: 48)
+                    .foregroundStyle(.white)
+                })
+                .background(Color(.systemBlue))
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                .padding(.top, 24)
+                Spacer()
+                
+                //sign up button
+                
+                NavigationLink {
+                    Text(verbatim: "asd")
+                } label: {
+                    HStack(spacing:3){
+                        Text("Don't have an account ? ")
+                        Text("Sign up ")
+                            .fontWeight(.bold)
+                    }
+                    .font(.system(size: 14))
+                }
+
+            }
+        }
+    }
+}
+
+#Preview {
+    LoginView()
+}
