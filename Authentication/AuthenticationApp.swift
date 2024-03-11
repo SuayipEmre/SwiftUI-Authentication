67a954538051fd3cda0b8e20b24429d8f65fd415
Suayip Emre <suayipemredev@gmail.com>
2024-03-11 21:38:45 +0300

    Initial Commit

---
+++ Authentication/AuthenticationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthenticationApp.swift
+//  Authentication
+//
+//  Created by Şuayip Emre on 11.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct AuthenticationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Authentication/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Authentication
+//
+//  Created by Şuayip Emre on 11.03.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
